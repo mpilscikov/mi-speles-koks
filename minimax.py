e427ed6ad897383d0e453ffc8a703abe64a1613b
raganu beka <m.pilscikov@gmail.com>
2024-04-01 12:38:55 +0300

    cleanup

--- minimax.py
+++ minimax.py
@@ -1,6 +1,7 @@
 import math
 
-class AlgorithmMinimax: 
+
+class AlgorithmMinimax:
 
     def __init__(self, move=None, value=None):
         self.move = move
@@ -8,24 +9,24 @@ class AlgorithmMinimax:
 
     def __str__(self):
         return f" {self.move}: {self.value}"
-    
-    def minimax(self,  node, is_max): 
+
+    def minimax(self,  node, is_max):
 
         if not node.children:
             return AlgorithmMinimax(None, node.heuristic_value)
 
-        if is_max: 
-            valMax = -math.inf 
+        if is_max:
+            value_max = -math.inf
 
             for child in node.children:
                 value = self.minimax(child, False).value
-                valMax = max(valMax,  value) 
-            return AlgorithmMinimax("max", valMax)  
-        
+                value_max = max(value_max,  value)
+            return AlgorithmMinimax("max", value_max)
+
         else:
             valMin = math.inf
 
             for child in node.children:
                 value = self.minimax(child, True).value
-                valMin =  min(valMin, value)
+                valMin = min(valMin, value)
             return AlgorithmMinimax("min", valMin)
