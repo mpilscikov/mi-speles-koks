0d7d264c7bda1e032a8fd450f9b2945f0ff0bd87
221RDB117 <114361013+AgneseRozenberga@users.noreply.github.com>
2024-04-07 23:58:17 +0300

    Update ai_move.py

--- ai_move.py
+++ ai_move.py
@@ -1,10 +1,34 @@
 from alpha_beta import AlphaBeta
 from tree_generator import GameState, GameTreeGenerator
 from constants import ALLOWED_DIVISORS
+from minimax import Minimax
 
 def ai_move(game_state, algorithm, player_first):
     if algorithm == "minimax":
-        pass
+        best_value = float("-inf")
+        best_move=None
+
+        for divisor in ALLOWED_DIVISORS:
+            if game_state.number % divisor== 0:
+                new_number= game_state.number // divisor
+                new_points, new_bank = GameTreeGenerator.calculate_points_and_bank(new_number,
+                                                                               game_state.points,
+                                                                               game_state.bank)
+                new_state = GameState(new_number, new_points, new_bank)
+                value = Minimax.minimax(GameTreeGenerator.generate_tree(new_state), depth=20,
+                                        maximizing_player=False, ai_player=2 if player_first == "Player" else 1)
+                if value >best_value:
+                    best_value = value
+                    best_move = divisor
+
+        if best_move is not None:
+            new_number = game_state.number // best_move
+            new_points,  new_bank = GameTreeGenerator.calculate_points_and_bank(new_number,
+                                                                               game_state.points,
+                                                                               game_state.bank)
+            game_state = GameState(new_number, new_points, new_bank)
+            return game_state
+            
     elif algorithm == "alpha_beta":
         best_value = float("-inf")
         best_move = None
