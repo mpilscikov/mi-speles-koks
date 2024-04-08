f2723c8fdb037f4318dcda3c010367aff3896a20
221RDB117 <114361013+AgneseRozenberga@users.noreply.github.com>
2024-04-08 21:05:27 +0300

    Update game_ui.py

--- game_ui.py
+++ game_ui.py
@@ -142,7 +142,6 @@ class GameUI:
 
     def show_authors(self):
         authors = "Authors:\nArtjoms Tatagaškins\nMaksims Piļščikovs\nAgnese Rozenberga\nAnce Valnere\nMaigurs Skangalis"
-
         messagebox.showinfo("Authors", authors)
 
     def update_labels(self):
